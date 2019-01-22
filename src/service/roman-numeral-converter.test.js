9d0cd0a018d9f7829be097cc2957ff7cacb68bcb
enzotoshio <enzo.toshiba@gmail.com>
2019-01-21 22:52:43 -0200

    feature: add service test scenarios

---
+++ src/service/roman-numeral-converter.test.js
@@ -0,0 +1,33 @@
+describe('romanNumeralConverter', () => {
+  describe('when not receiving a parameter', () => {
+    it('returns an exception', () => {});
+  });
+
+  describe('when not receiving a number', () => {
+    it('returns an exception', () => {});
+  });
+
+  describe('when receiving a number above 3.999.999', () => {
+    it('returns an exception', () => {});
+  });
+
+  describe('when receiving a number below 0', () => {
+    it('returns an exception', () => {});
+  });
+
+  describe('when receiving a number above 3.999', () => {
+    it('returns the correct numeral', () => {});
+  });
+
+  describe('when receiving a number below or equal 3.999', () => {
+    it('returns the correct numeral', () => {});
+  });
+
+  describe('when receiving a number that represents four times a unit', () => {
+    it('returns a composed numeral instead of repeating four times', () => {});
+  });
+
+  describe('when receiving a number that represents three times a unit', () => {
+    it('returns a composed numeral instead of repeating four times', () => {});
+  });
+});
